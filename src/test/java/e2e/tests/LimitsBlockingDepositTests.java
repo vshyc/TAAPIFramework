8b357cdcb9caa6dc5e4631c0f1ffb0060db133c4
Dariusz Szyca <dariusz_szyca@epam.com>
2021-10-15 10:57:53 +0200

    DOTA-2559 add system propertied

--- src/test/java/e2e/tests/LimitsBlockingDepositTests.java
+++ src/test/java/e2e/tests/LimitsBlockingDepositTests.java
@@ -77,7 +77,7 @@ public class LimitsBlockingDepositTests extends BaseTest {
     }
 
     @Test
-    @TestCaseId(5610922)
+    @TestCaseId(3445)
     @DisplayName("Check if AML Limit is blocking deposit higher then 100 on staging or 150 on TTS")
     public void checkIfAmlLimitIsBlockingDepositIfHigherThenAMLLimit() {
         Response paymentResponse = paymentHelper.payIn(sessionId, jsession, slaveId, "de",
