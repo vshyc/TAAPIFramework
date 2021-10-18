a9d98ee8ca36f60865e292da12b968c4a059a04b
Dariusz Szyca <dariusz_szyca@epam.com>
2021-10-18 16:13:04 +0200

    DOTA-2559 add constant for path

--- src/main/java/customer/stake/constants/RequestPaths.java
+++ src/main/java/customer/stake/constants/RequestPaths.java
@@ -3,5 +3,5 @@ package customer.stake.constants;
 public class RequestPaths {
 
     public static final String LIMITS_REQUEST_PATH = "/customer/limits";
-    public static final String LIMIT_HISTORY_REQUEST_PATH = "/customer/limits/history";
+    public static final String LIMIT_HISTORY_REQUEST_PATH = LIMITS_REQUEST_PATH + "/history";
 }
