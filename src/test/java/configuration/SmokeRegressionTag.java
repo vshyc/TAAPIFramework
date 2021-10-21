a0a05b8bc20c6ba8f3b4dcd49b6dddd3120cd650
Dariusz Szyca <dariusz_szyca@epam.com>
2021-10-21 13:34:55 +0200

    DOTA-2559 remove Test Tag from interface class

--- src/test/java/configuration/SmokeRegressionTag.java
+++ src/test/java/configuration/SmokeRegressionTag.java
@@ -1,7 +1,6 @@
 package configuration;
 
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,6 +10,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Tag("SmokeTests") @Tag("RegressionTests")
-@Test
 public @interface SmokeRegressionTag {
 }
