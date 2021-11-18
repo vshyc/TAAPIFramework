c93337b1f0a98e95b3503105b55cd54312e49a9b
Aneliya Duhalova <Aneliya_Duhalova@epam.com>
2021-11-18 13:43:48 +0200

    DOTA-2633 comments resolving

---
+++ src/main/java/customer/stake/dto/rgfes/RGFESPanicButtonResponse.java
@@ -0,0 +1,10 @@
+package customer.stake.dto.rgfes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RGFESPanicButtonResponse {
+
+}
--- src/main/java/customer/stake/rop/PostRGFESPanicBtnEndPoint.java
+++ src/main/java/customer/stake/rop/PostRGFESPanicBtnEndPoint.java
@@ -4,20 +4,20 @@ import static customer.stake.constants.RequestPaths.PANIC_BUTTON_GAMES_PATH;
 import static customer.stake.constants.RequestPaths.PANIC_BUTTON_MYSINO_PATH;
 import static io.restassured.RestAssured.given;
 
-import customer.stake.dto.cus.AccountStatusResponseCAS;
+import customer.stake.dto.rgfes.RGFESPanicButtonResponse;
 import customer.stake.helpers.HelpersConfig;
 import customer.stake.properties.EnvConfig;
 import java.lang.reflect.Type;
 import org.apache.http.HttpStatus;
 
 
-public class PostRGFESPanicBtnEndPoint extends BaseEndpoint<PostRGFESPanicBtnEndPoint, AccountStatusResponseCAS> {
+public class PostRGFESPanicBtnEndPoint extends BaseEndpoint<PostRGFESPanicBtnEndPoint, RGFESPanicButtonResponse> {
 
     private EnvConfig envConfig = HelpersConfig.createConfiguration();
 
     @Override
     protected Type getModelType() {
-        return AccountStatusResponseCAS.class;
+        return RGFESPanicButtonResponse.class;
     }
 
     @Override
